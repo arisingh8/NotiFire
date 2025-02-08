bb9083b6e67bf8be561c8ea21d560f8dda1d8d1a
Pratham Ingale <pratham.ing49@gmail.com>
2025-02-07 20:55:19 -0500

    bigger header font size

--- frontend/fireguard/src/app/sections/header/styles.ts
+++ frontend/fireguard/src/app/sections/header/styles.ts
@@ -3,7 +3,7 @@ export const HeaderStyles = {
   container: "fixed top-0 left-0 right-0 w-full bg-gray-800 text-white py-4 shadow-md z-50",
   wrapper: "w-full px-8",
   nav: "flex items-center justify-between max-w-7xl mx-auto",
-  logo: "text-[#ffdbbb] text-xl font-bold hover:opacity-80",
+  logo: "text-[#ffdbbb] text-3xl font-bold hover:opacity-80",
   links: "flex items-center space-x-4",
-  link: "text-[#ffdbbb] hover:opacity-80 text-sm"
+  link: "text-[#ffdbbb] hover:opacity-80 text-3x1"
 };

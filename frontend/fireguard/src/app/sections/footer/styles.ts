3e62bc1bda47b7aa5c1bec5a702d3ef80297708c
Pratham Ingale <pratham.ing49@gmail.com>
2025-02-07 20:49:43 -0500

    minor head & foot tweaks

--- frontend/fireguard/src/app/sections/footer/styles.ts
+++ frontend/fireguard/src/app/sections/footer/styles.ts
@@ -1,12 +1,12 @@
 export const FooterStyles = {
-    font: "font-[family-name:var(--font-eb-garamond)]",
-    container: "fixed bottom-0 left-0 w-full bg-gray-800 text-[#ffdbbb] py-4 shadow-md z-50",
-    wrapper: "container mx-auto",
-    content: "flex items-center justify-between",
-    copyright: "text-sm text-[#ffdbbb]",
-    nav: "flex space-x-4",
-    link: "text-sm text-[#ffdbbb] hover:opacity-80",
-    divider: "text-[#ffdbbb]",
-    socialIcons: "flex space-x-4",
-    icon: "text-[#ffdbbb] hover:opacity-80"
-  };
+  font: "font-[family-name:var(--font-eb-garamond)]",
+  container: "fixed bottom-0 left-0 right-0 w-full bg-gray-800 text-[#ffdbbb] py-4 shadow-md z-50",
+  wrapper: "w-full px-8",
+  content: "flex items-center justify-between max-w-7xl mx-auto",
+  copyright: "text-sm text-[#ffdbbb]",
+  nav: "flex space-x-4",
+  link: "text-sm text-[#ffdbbb] hover:opacity-80",
+  divider: "text-[#ffdbbb]",
+  socialIcons: "flex space-x-4",
+  icon: "text-[#ffdbbb] hover:opacity-80"
+};

8caf70f2a464947d6fc8a276619a016fc160339b
Pratham Ingale <pratham.ing49@gmail.com>
2025-02-08 16:16:34 -0500

    profile

--- frontend/fireguard/src/app/profile/page.tsx
+++ frontend/fireguard/src/app/profile/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 import Profile from '@/app/sections/profile';
 
 export default function ProfileTest() {

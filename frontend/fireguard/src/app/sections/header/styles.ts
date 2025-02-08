da1de1d9575244c78259f7a8116ba99f3ac4c652
Pratham Ingale <pratham.ing49@gmail.com>
2025-02-07 20:42:41 -0500

    new fonts

---
+++ frontend/fireguard/src/app/sections/header/styles.ts
@@ -0,0 +1,9 @@
+export const HeaderStyles = {
+    font: "font-[family-name:var(--font-eb-garamond)]",
+    container: "fixed top-0 left-0 w-full bg-gray-800 text-white py-4 shadow-md z-50",
+    wrapper: "container mx-auto",
+    nav: "flex items-center justify-between",
+    logo: "text-[#ffdbbb] text-xl font-bold hover:opacity-80",
+    links: "flex items-center space-x-4",
+    link: "text-[#ffdbbb] hover:opacity-80 text-sm"
+  };

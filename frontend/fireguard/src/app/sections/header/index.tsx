9ae563d6ca21121b1d6c57c4f8c0a8242067605f
Pratham Ingale <pratham.ing49@gmail.com>
2025-02-08 16:00:03 -0500

    added dashboard logic to header

--- frontend/fireguard/src/app/sections/header/index.tsx
+++ frontend/fireguard/src/app/sections/header/index.tsx
@@ -40,18 +40,24 @@ const Header: React.FC = () => {
           <div className={HeaderStyles.links}>
             <Link href="/" className={HeaderStyles.link}>
               Home
-            </Link>
-            <Link href="/about" className={HeaderStyles.link}>
-              About
-            </Link>
-            <Link href="/contact" className={HeaderStyles.link}>
-              Contact
-            </Link>
-            {userRole ? (
-              <>
+            </Link>{userRole ? (
+              <>{(() => {
+                  switch (userRole) {
+                    case "resident":
+                      return <Link href="/resident/dashboard" className={HeaderStyles.link}>Dashboard</Link>;
+                    case "dispatcher":
+                      return <Link href="/dispatcher/dashboard" className={HeaderStyles.link}>Dashboard</Link>;
+                    case "first_responder":
+                      return <Link href="/firstresponder/dashboard" className={HeaderStyles.link}>Dashboard</Link>;
+                    default:
+                      return <Link href="/" className={HeaderStyles.link}>Dashboard</Link>;
+                  }
+                })()}
+
                 <Link href="/profile" className={HeaderStyles.link}>
                   Profile
                 </Link>
+                
                 <button onClick={handleLogout} className={HeaderStyles.link}>
                   Log Out
                 </button>
@@ -61,6 +67,13 @@ const Header: React.FC = () => {
                 Log In
               </Link>
             )}
+            <Link href="/about" className={HeaderStyles.link}>
+              About
+            </Link>
+            <Link href="/contact" className={HeaderStyles.link}>
+              Contact
+            </Link>
+            
           </div>
         </div>
       </div>

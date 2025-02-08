74541ccb87e080f397812e40570878226789d995
Pratham Ingale <pratham.ing49@gmail.com>
2025-02-07 20:49:43 -0500

    minor head & foot tweaks

--- frontend/fireguard/src/app/sections/header/styles.ts
+++ frontend/fireguard/src/app/sections/header/styles.ts
@@ -1,9 +1,9 @@
 export const HeaderStyles = {
-    font: "font-[family-name:var(--font-eb-garamond)]",
-    container: "fixed top-0 left-0 w-full bg-gray-800 text-white py-4 shadow-md z-50",
-    wrapper: "container mx-auto",
-    nav: "flex items-center justify-between",
-    logo: "text-[#ffdbbb] text-xl font-bold hover:opacity-80",
-    links: "flex items-center space-x-4",
-    link: "text-[#ffdbbb] hover:opacity-80 text-sm"
-  };
+  font: "font-[family-name:var(--font-eb-garamond)]",
+  container: "fixed top-0 left-0 right-0 w-full bg-gray-800 text-white py-4 shadow-md z-50",
+  wrapper: "w-full px-8",
+  nav: "flex items-center justify-between max-w-7xl mx-auto",
+  logo: "text-[#ffdbbb] text-xl font-bold hover:opacity-80",
+  links: "flex items-center space-x-4",
+  link: "text-[#ffdbbb] hover:opacity-80 text-sm"
+};

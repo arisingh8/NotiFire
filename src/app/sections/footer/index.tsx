631a29a14eed7da3c39ada1c5a07ea2c7ada8ff1
Ari Singh <ari.singh@duke.edu>
2025-07-08 16:08:03 -0400

    cleaning up footer

--- src/app/sections/footer/index.tsx
+++ src/app/sections/footer/index.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Link from "next/link";
-import { Facebook, Twitter, Instagram } from "lucide-react";
 import { FooterStyles } from "./styles";
 
 const Footer: React.FC = () => {
@@ -13,42 +11,6 @@ const Footer: React.FC = () => {
           <p className={FooterStyles.copyright}>
             &copy; {new Date().getFullYear()} NotifireAI
           </p>
-
-          {/* Navigation Links */}
-          <nav className={FooterStyles.nav}>
-            <Link href="/privacy-policy" className={FooterStyles.link}>
-              Privacy Policy
-            </Link>
-            <span className={FooterStyles.divider}>|</span>
-            <Link href="/terms-of-service" className={FooterStyles.link}>
-              Terms of Service
-            </Link>
-          </nav>
-
-          {/* Social Media Icons */}
-          <div className={FooterStyles.socialIcons}>
-            <Link
-              href="https://facebook.com"
-              aria-label="Facebook"
-              target="_blank"
-            >
-              <Facebook size={20} className={FooterStyles.icon} />
-            </Link>
-            <Link
-              href="https://twitter.com"
-              aria-label="Twitter"
-              target="_blank"
-            >
-              <Twitter size={20} className={FooterStyles.icon} />
-            </Link>
-            <Link
-              href="https://instagram.com"
-              aria-label="Instagram"
-              target="_blank"
-            >
-              <Instagram size={20} className={FooterStyles.icon} />
-            </Link>
-          </div>
         </div>
       </div>
     </footer>
